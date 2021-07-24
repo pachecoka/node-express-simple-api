afd52baceaf7b87a45bd21f59166c02baf64a265
Karolina Pacheco <karolina_cris@hotmail.com>
2021-07-24 20:31:51 -0300

    add post, refactoring

--- src/app.js
+++ src/app.js
@@ -1,34 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 
-bookRouter.route('/books').get((req, res) => {
-  const query = {};
-  if (req.query.genre) {
-    query.genre = req.query.genre;
-  }
-
-  Book.find(query, (err, books) => {
-    if (err) {
-      return res.send(err);
-    }
-    return res.json(books);
-  });
-});
-
-bookRouter.route('/books/:bookId').get((req, res) => {
-  Book.findById(req.params.bookId, (err, book) => {
-    if (err) {
-      return res.send(err);
-    }
-    return res.json(book);
-  });
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use('/api', bookRouter);
 
